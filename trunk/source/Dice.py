629434b4e7b6334303347f06d917c79ed8f03494
March.Liu <March.Liu@a0e12bcf-9d19-0410-93c5-753707fc8146>
2006-08-09 15:00:49 +0000

    The static method Dice.Dice.make now become a modoul method Dice.MakeDice.
    Add Dice.txt and Player.txt

--- trunk/source/Dice.py
+++ trunk/source/Dice.py
@@ -6,15 +6,13 @@ class Dice(object):
         self.min = 1
         self.max = faces 
 
-    @staticmethod
-    def make(minPoint, maxPoint):
-        re = Dict(MaxPoint)
-        re.min = minPoint
-
     def Roll(self, times = 1):
         re = 0
         for i in xrange(times):
             re += random.randint(self.min, self.max)
         return re
 
+def MakeDice(minPoint, maxPoint):
+    re = Dict(MaxPoint)
+    re.min = minPoint
     
